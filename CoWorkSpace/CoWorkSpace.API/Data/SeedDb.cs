a35b7ad1f523249e45f7c919a33559112ba6be3c
dzuluaga23 <david12-zuluaga@hotmail.com>
2024-10-06 13:05:57 -0500

    seedDb

---
+++ CoWorkSpace/CoWorkSpace.API/Data/SeedDb.cs
@@ -0,0 +1,31 @@
+﻿using CoWorkSpace.API.Data;
+using CoWorkSpace.Shared.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoWorkSpace.Api.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckMembresiaAsync();
+        }
+
+        public async Task CheckMembresiaAsync()
+        {
+            if (!_context.Membresias.Any())
+            {
+                _context.Membresias.Add(new Membresia { Tipo = "Premium" });
+                _context.Membresias.Add(new Membresia { Tipo = "Estándar" });
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
